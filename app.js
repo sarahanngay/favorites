68b122b8dc49c449093e11428972e6e1e3b2d8f6
sarahbobber <sarah@brandingbrand.com>
2014-05-08 23:10:45 -0400

    added listen

--- app.js
+++ app.js
@@ -9,6 +9,4 @@
   app.use(require('body-parser')());              // pull information from html in POST
   app.use(require('method-override')());            // simulate DELETE and PUT
 
-  // listen (start app with node server.js) ======================================
-  app.listen(8080);
-  console.log("App listening on port 8080");
+  app.listen(4000);
