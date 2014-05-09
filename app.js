50ba060388ab4068b8b7782bcea1fcbe1f7b4170
Sarah Gay <sarah@brandingbrand.com>
2014-05-09 10:42:47 -0400

    small updates

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
-  var express  = require('express');
-  var app      = express();
-  var mongoose = require('mongoose');
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var logfmt   = require('logfmt');
 
-  mongoose.connect('mongodb://sarahbobber:dumble11@ds031627.mongolab.com:31627/favorites');   // connect to mongoDB database on modulus.io
+mongoose.connect('mongodb://sarahbobber:dumble11@ds031627.mongolab.com:31627/favorites');   // connect to mongoDB database on modulus.io
 
-  app.use(express.static(__dirname + '/public/index'));     // set the static files location /public/img will be /img for users
-  app.use(require('body-parser')());              // pull information from html in POST
-  app.use(require('method-override')());            // simulate DELETE and PUT
+app.use(logfmt.requestLogger());
 
-  app.listen(4000);
+app.use(express.static(__dirname + '/public/index'));
+app.use(require('body-parser')());
+app.use(require('method-override')());
 
-  app.get('/', function (req, res) {
-    res.send({hello: 'world'})
-  });
+app.get('/', function (req, res) {
+  res.send({hello: 'world'})
+});
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
