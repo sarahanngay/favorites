9879a3e76ab2ee79097c167f7c5448f8a95b3ff8
sarahbobber <sarah@brandingbrand.com>
2014-05-09 09:38:32 -0400

    testing herokuapp

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 
   mongoose.connect('mongodb://sarahbobber:dumble11@ds031627.mongolab.com:31627/favorites');   // connect to mongoDB database on modulus.io
 
-  app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
+  app.use(express.static(__dirname + '/public/index'));     // set the static files location /public/img will be /img for users
   app.use(require('body-parser')());              // pull information from html in POST
   app.use(require('method-override')());            // simulate DELETE and PUT
 
-  app.listen(4000);
+  app.get('/', res.send({hello: 'world'});
